ca6d22ae3111e04f7177f997e5f03222e2318e8d
Luthfulahi <luteeph@gmail.com>
2024-01-07 19:32:59 +0400

    initialize verisync.js

---
+++ index.js
@@ -0,0 +1,22 @@
+class Verisync {
+    config;
+    appId;
+
+    constructor(config) {
+        this.config = config;
+        this.appId = encodeURIComponent(
+            JSON.stringify({
+                redirectUrl: config.redirectUrl,
+                clientId: config.clientId,
+                flowId: config.flowId,
+                meta: config.meta,
+            })
+        );
+    }
+
+    verify() {
+        window.open(`https://app.verisync.co/synchroniser/${this.appId}`, "_blank");
+    }
+}
+
+export default Verisync;
